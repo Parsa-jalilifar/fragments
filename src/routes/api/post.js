2fc4e28ee70f8f27038ce986db3acdfbd9e6e7ee
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-06-11 15:02:30 -0400

    Add location to the post

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -2,19 +2,28 @@ const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const logger = require('../../logger');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   try {
-    const fragment = new Fragment({ ownerId: req.user, type: req.headers['content-type'] });
+    if (!Fragment.isSupportedType(req.headers['content-type'])) {
+      throw new Error('Invalid type');
+    }
+
+    const fragment = new Fragment({
+      ownerId: req.user,
+      type: req.headers['content-type'],
+      size: Buffer.byteLength(req.headers['content-type']),
+    });
+
     fragment.save();
     fragment.setData(req.body);
+
+    const location = `${process.env.API_URL}/v1/fragments/${fragment.id}`;
+    res.location(location);
 
     res.status(201).json(createSuccessResponse({ fragment }));
   } catch (error) {
     const status = error.status || 500;
-
-    if (status > 499) {
-      logger.error(error.message, `Error processing request`);
-    }
+    if (status > 499) logger.error(error.message, `Error processing request`);
     res.status(status).json(createErrorResponse(status, error.message));
   }
 };
