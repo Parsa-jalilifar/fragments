7fd9040930b5924913feea0e5383b239845beb3a
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-06-11 18:36:17 -0400

    Remove extra code

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,29 +1,22 @@
 const { Fragment } = require('../../model/fragment');
-const { createSuccessResponse, createErrorResponse } = require('../../response');
-const logger = require('../../logger');
+const { createSuccessResponse } = require('../../response');
 
 module.exports = async (req, res) => {
-  try {
-    if (!Fragment.isSupportedType(req.headers['content-type'])) {
-      throw new Error('Invalid type');
-    }
-
-    const fragment = new Fragment({
-      ownerId: req.user,
-      type: req.headers['content-type'],
-      size: Buffer.byteLength(req.headers['content-type']),
-    });
-
-    fragment.save();
-    fragment.setData(req.body);
-
-    const location = `${process.env.API_URL}/v1/fragments/${fragment.id}`;
-    res.location(location);
-
-    res.status(201).json(createSuccessResponse({ fragment }));
-  } catch (error) {
-    const status = error.status || 500;
-    if (status > 499) logger.error(error.message, `Error processing request`);
-    res.status(status).json(createErrorResponse(status, error.message));
+  if (!Fragment.isSupportedType(req.headers['content-type'])) {
+    throw new Error('Invalid type');
   }
+
+  const fragment = new Fragment({
+    ownerId: req.user,
+    type: req.headers['content-type'],
+    size: Buffer.byteLength(req.headers['content-type']),
+  });
+
+  fragment.save();
+  fragment.setData(req.body);
+
+  const location = `${process.env.API_URL}/v1/fragments/${fragment.id}`;
+  res.location(location);
+
+  res.status(201).json(createSuccessResponse({ fragment }));
 };
