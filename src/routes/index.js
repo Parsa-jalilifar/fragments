d91434da410126b5672548b28acc4fb97a3f2f38
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-06-11 16:53:19 -0400

    Add hashEmail

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,17 +7,24 @@ const { version, author } = require('../../package.json');
 
 // Our authorization middleware
 const { authenticate } = require('../authorization');
+const hash = require('../hash');
 
 const { createSuccessResponse } = require('../response');
 
 // Create a router that we can use to mount our API
 const router = express.Router();
 
+// Hashing Email Address
+const hashEmail = async (req, res, next) => {
+  req.user = hash(req.user);
+  next();
+};
+
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  * Protect them all so you have to be authenticated in order to access.
  */
-router.use(`/v1`, authenticate(), require('./api'));
+router.use(`/v1`, authenticate(), hashEmail, require('./api'));
 
 /**
  * Define a simple health check route. If the server is running
