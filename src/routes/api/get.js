ccb3c8f0c71a046b3cb8c46147a1ac58178ab4eb
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-06-10 23:00:39 -0400

    Update to show all fragments of a user

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,12 +1,12 @@
 // src/routes/api/get.js
 
 const { createSuccessResponse } = require('../../response');
+const { listFragments } = require('../../model/data/memory');
 
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-  // TODO: this is just a placeholder to get something working...
-  const data = { fragments: [] };
-  res.status(200).json(createSuccessResponse(data));
+module.exports = async (req, res) => {
+  const fragments = await listFragments(req.user);
+  res.status(200).json(createSuccessResponse({ fragments }));
 };
