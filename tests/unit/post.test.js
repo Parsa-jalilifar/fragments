eddf7724943e16483c0f336dc106e7ed0269256d
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-06-17 13:02:06 -0400

    Add missing the Authorization header test

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -3,14 +3,17 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
-describe('POST /fragments', () => {
-  test('authenticated users can create a plain text fragment', async () => {
+describe('POST /v1/fragments', () => {
+  test('authenticated users can create a fragment', async () => {
     const res = await request(app).post('/v1/fragments').auth('user1@email.com', 'password1').send({
       body: 'This is a fragment',
     });
     expect(res.statusCode).toBe(201);
     expect(res.body.status).toBe('ok');
   });
+
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () => request(app).post('/v1/fragments').expect(401));
 
   // If the wrong username/password pair are used (no such user), it should be forbidden
   test('incorrect credentials are denied', () =>
