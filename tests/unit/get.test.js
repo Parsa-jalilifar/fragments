b9561bbd995a45e9958f43720b2c92b4182fa5d7
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-06-17 15:02:52 -0400

    Add tests for GET /v1/fragments/:id/info

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -21,3 +21,41 @@ describe('GET /v1/fragments', () => {
 
   // TODO: we'll need to add tests to check the contents of the fragments array later
 });
+
+describe('GET /v1/fragments/:id/info', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () =>
+    request(app).get('/v1/fragments/id/info').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app)
+      .get('/v1/fragments/:id/info')
+      .auth('invalid@email.com', 'incorrect_password')
+      .expect(401));
+
+  test('requested fragment metadata by id should match with posted fragment', async () => {
+    const postRequest = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set({
+        'Content-Type': 'text/plain',
+        body: 'This is a fragment',
+      });
+
+    const getRequest = await request(app)
+      .get(`/v1/fragments/${postRequest.body.fragment.id}/info`)
+      .auth('user1@email.com', 'password1');
+
+    expect(getRequest.statusCode).toBe(200);
+    expect(getRequest.body.status).toBe('ok');
+  });
+
+  test('requested fragment metadata by id should exist', async () => {
+    const res = await request(app)
+      .get(`/v1/fragments/radomId/info`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.statusCode).toBe(500);
+  });
+});
