bf0389cb36d08c498770672b092177d57ae3299d
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-06-17 21:04:49 -0400

    Add tests for GET /v1/fragments/:id

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -22,6 +22,43 @@ describe('GET /v1/fragments', () => {
   // TODO: we'll need to add tests to check the contents of the fragments array later
 });
 
+describe('GET /v1/fragments/:id', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () =>
+    request(app).get('/v1/fragments/id').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app)
+      .get('/v1/fragments/id')
+      .auth('invalid@email.com', 'incorrect_password')
+      .expect(401));
+
+  test('data of posted fragment can be retrieved by id', async () => {
+    const postResponse = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set({
+        'Content-Type': 'text/plain',
+        body: 'This is a fragment',
+      });
+
+    const getResponse = await request(app)
+      .get(`/v1/fragments/${postResponse.body.fragment.id}`)
+      .auth('user1@email.com', 'password1');
+
+    expect(getResponse.statusCode).toBe(200);
+  });
+
+  test('requested fragment data by invalid id should fail', async () => {
+    const res = await request(app)
+      .get(`/v1/fragments/randomId`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.statusCode).toBe(500);
+  });
+});
+
 describe('GET /v1/fragments/:id/info', () => {
   // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () =>
@@ -34,8 +71,8 @@ describe('GET /v1/fragments/:id/info', () => {
       .auth('invalid@email.com', 'incorrect_password')
       .expect(401));
 
-  test('requested fragment metadata by id should match with posted fragment', async () => {
-    const postRequest = await request(app)
+  test('metadata of posted fragment can be retrieved by id', async () => {
+    const postResponse = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
       .set({
@@ -43,17 +80,17 @@ describe('GET /v1/fragments/:id/info', () => {
         body: 'This is a fragment',
       });
 
-    const getRequest = await request(app)
-      .get(`/v1/fragments/${postRequest.body.fragment.id}/info`)
+    const getResponse = await request(app)
+      .get(`/v1/fragments/${postResponse.body.fragment.id}/info`)
       .auth('user1@email.com', 'password1');
 
-    expect(getRequest.statusCode).toBe(200);
-    expect(getRequest.body.status).toBe('ok');
+    expect(getResponse.statusCode).toBe(200);
+    expect(getResponse.body.status).toBe('ok');
   });
 
-  test('requested fragment metadata by id should exist', async () => {
+  test('requested fragment metadata by invalid id should fail', async () => {
     const res = await request(app)
-      .get(`/v1/fragments/radomId/info`)
+      .get(`/v1/fragments/randomId/info`)
       .auth('user1@email.com', 'password1');
 
     expect(res.statusCode).toBe(500);
