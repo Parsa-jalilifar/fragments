5d39004ded81c6cad6ebc8f9caead76a92bf6446
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-06-19 13:26:02 -0400

    Add GET /fragments/:id.ext

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,5 +1,7 @@
 // src/routes/api/get.js
 
+const path = require('path');
+
 const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const logger = require('../../logger');
@@ -12,19 +14,43 @@ module.exports = {
       logger.debug({ fragments }, 'GET /fragments');
       res.status(200).json(createSuccessResponse({ fragments }));
     } catch (error) {
-      res.status(500).json(createErrorResponse(500, error.message));
+      res.status(404).json(createErrorResponse(404, error.message));
     }
   },
 
   // Get a fragment data by using its id
   fragmentDataById: async (req, res) => {
     try {
-      const fragment = await Fragment.byId(req.user, req.params.id);
+      const ownerId = req.user;
+      const id = path.parse(req.params.id).name;
+      const ext = path.parse(req.params.id).ext.slice(1);
+
+      const fragment = await Fragment.byId(ownerId, id);
       const fragmentData = await fragment.getData();
-      logger.debug({ fragment }, 'GET /fragments/:id');
-      res.status(200).send(fragmentData.toString());
+
+      if (!ext) {
+        logger.debug({ fragment }, 'GET /fragments/:id');
+        res.status(200).send(fragmentData.toString());
+      } else {
+        logger.debug({ id: id, ext: ext }, 'GET /fragments/:id.ext');
+
+        if (!Fragment.isSupportedExt(ext)) {
+          return res.status(415).json(createErrorResponse(415, 'Extension type is not supported.'));
+        }
+
+        const type = Fragment.extValidType(ext); // html -> text/html
+
+        if (!fragment.formats.includes(type)) {
+          return res.status(415).json(createErrorResponse(415, 'Conversion is not allowed.'));
+        }
+
+        const newFragmentData = fragment.convertData(fragmentData, type);
+        logger.debug({ newFragmentData }, 'Converted fragment data');
+        res.setHeader('Content-type', type);
+        res.status(200).send(newFragmentData);
+      }
     } catch (error) {
-      res.status(500).json(createErrorResponse(500, error.message));
+      res.status(404).json(createErrorResponse(404, error.message));
     }
   },
 
@@ -35,7 +61,7 @@ module.exports = {
       logger.debug({ fragment }, 'GET /fragments/:id/info');
       res.status(200).json(createSuccessResponse(fragment));
     } catch (error) {
-      res.status(500).json(createErrorResponse(500, error.message));
+      res.status(404).json(createErrorResponse(404, error.message));
     }
   },
 };
