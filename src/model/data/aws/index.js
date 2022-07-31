06e8f09622a868cd0733e340eb802460ed888ee4
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-07-31 18:59:28 -0400

    Test for fragment is not saved in S3

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -2,10 +2,32 @@
 const MemoryDB = require('../memory/memory-db');
 const s3Client = require('./s3Client');
 const { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
-const logger = require('../../../logger');
+const { logger } = require('../../../logger');
 
 // Create two in-memory databases: one for fragment metadata and the other for raw data
+const data = new MemoryDB();
 const metadata = new MemoryDB();
+
+// Convert a stream of data into a Buffer, by collecting
+// chunks of data until finished, then assembling them together.
+// We wrap the whole thing in a Promise so it's easier to consume.
+const streamToBuffer = (stream) =>
+  new Promise((resolve, reject) => {
+    // As the data streams in, we'll collect it into an array.
+    const chunks = [];
+
+    // Streams have events that we can listen for and run
+    // code.  We need to know when new `data` is available,
+    // if there's an `error`, and when we're at the `end`
+    // of the stream.
+
+    // When there's data, add the chunk to our chunks list
+    stream.on('data', (chunk) => chunks.push(chunk));
+    // When there's an error, reject the Promise
+    stream.on('error', reject);
+    // When the stream is done, resolve with a new Buffer of our chunks
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
+  });
 
 // Write a fragment's metadata to memory db. Returns a Promise
 function writeFragment(fragment) {
@@ -41,27 +63,6 @@ async function writeFragmentData(ownerId, id, data) {
     throw new Error('unable to upload fragment data');
   }
 }
-
-// Convert a stream of data into a Buffer, by collecting
-// chunks of data until finished, then assembling them together.
-// We wrap the whole thing in a Promise so it's easier to consume.
-const streamToBuffer = (stream) =>
-  new Promise((resolve, reject) => {
-    // As the data streams in, we'll collect it into an array.
-    const chunks = [];
-
-    // Streams have events that we can listen for and run
-    // code.  We need to know when new `data` is available,
-    // if there's an `error`, and when we're at the `end`
-    // of the stream.
-
-    // When there's data, add the chunk to our chunks list
-    stream.on('data', (chunk) => chunks.push(chunk));
-    // When there's an error, reject the Promise
-    stream.on('error', reject);
-    // When the stream is done, resolve with a new Buffer of our chunks
-    stream.on('end', () => resolve(Buffer.concat(chunks)));
-  });
 
 // Reads a fragment's data from S3 and returns (Promise<Buffer>)
 // https://github.com/awsdocs/aws-sdk-for-javascript-v3/blob/main/doc_source/s3-example-creating-buckets.md#getting-a-file-from-an-amazon-s3-bucket
@@ -101,23 +102,28 @@ async function listFragments(ownerId, expand = false) {
   return fragments.map((fragment) => fragment.id);
 }
 
-// Delete a fragment's metadata and data from the Amazon S3 bucket
+// Delete a fragment's metadata and data from memory db. Returns a Promise
 async function deleteFragment(ownerId, id) {
-  // Create the PUT API params from our details
   const params = {
     Bucket: process.env.AWS_S3_BUCKET_NAME,
     // Our key will be a mix of the ownerID and fragment id, written as a path
     Key: `${ownerId}/${id}`,
+    Body: data,
   };
 
+  // create a DELETE object command to send to S3
   const command = new DeleteObjectCommand(params);
 
   try {
+    // Use our client to send the command
     await s3Client.send(command);
+    // Delete the metadata from the in-memory DB
+    await metadata.delete(ownerId, id);
   } catch (err) {
+    // If anything goes wrong, log enough info that we can debug
     const { Bucket, Key } = params;
-    logger.error({ err, Bucket, Key }, 'Error deleting fragment data from S3');
-    throw new Error('unable to delete fragment data');
+    logger.error({ err, Bucket, Key }, 'Error uploading fragment data to S3');
+    throw new Error('unable to upload fragment data');
   }
 }
 
