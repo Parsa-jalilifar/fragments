905329ac31082e51940d92c88554a5e3077cc0ea
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-08-03 23:00:08 -0400

    Add missing curly bracket

--- src/routes/api/get-id-info.js
+++ src/routes/api/get-id-info.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
   try {
     const fragment = await Fragment.byId(req.user, req.params.id);
     logger.debug({ fragment }, 'GET /fragments/:id/info');
-    res.status(200).json(createSuccessResponse(fragment));
+    res.status(200).json(createSuccessResponse({ fragment }));
   } catch (error) {
     res.status(404).json(createErrorResponse(404, error.message));
   }
